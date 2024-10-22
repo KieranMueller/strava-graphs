e37960236c8c844efed8ff2cc1257d577aa4e268
KieranMueller <kieran98mueller@gmail.com>
2024-10-22 11:43:56 -0500

    test

--- src/environment.ts
+++ src/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-    production: false,
+    production: true,
     CLIENT_ID: (window as any).__env?.CLIENT_ID || 'yo1',
     CLIENT_SECRET: (window as any).__env?.CLIENT_SECRET || 'yo2'
 }
