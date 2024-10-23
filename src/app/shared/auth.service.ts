186d48ac4685de57b53e15ed8dea8fae40a7ca9e
KieranMueller <kieran98mueller@gmail.com>
2024-10-22 23:21:38 -0500

    environments

--- src/app/shared/auth.service.ts
+++ src/app/shared/auth.service.ts
@@ -1,33 +1,33 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http'
-import { Injectable } from '@angular/core';
+import { Injectable } from '@angular/core'
 import { catchError, map, Observable, of } from 'rxjs'
 import { STRAVA_BASE_URL } from './env'
-import { environment } from '../../environment'
+import { environment } from '../../environments/environment'
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class AuthService {
 
-  constructor(private http: HttpClient) { }
+    constructor(private http: HttpClient) { }
 
-  exchangeAuthCodeForTokenData(code: string): Observable<Object> {
-    const formData = new FormData()
-    formData.append('client_id', environment.CLIENT_ID)
-    formData.append('client_secret', environment.CLIENT_SECRET)
-    formData.append('code', code)
-    formData.append('grant_type', 'authorization_code')
+    exchangeAuthCodeForTokenData(code: string): Observable<Object> {
+        const formData = new FormData()
+        formData.append('client_id', environment.CLIENT_ID)
+        formData.append('client_secret', environment.CLIENT_SECRET)
+        formData.append('code', code)
+        formData.append('grant_type', 'authorization_code')
 
-    return this.http.post(`${STRAVA_BASE_URL}/oauth/token`, formData)
-  }
+        return this.http.post(`${STRAVA_BASE_URL}/oauth/token`, formData)
+    }
 
-  isAuthenticated(token: string): Observable<boolean> {
-    const headers = new HttpHeaders({
-        Authorization: `Bearer ${token}`
-    })
-    return this.http.get(`${STRAVA_BASE_URL}/api/v3/athlete`, {headers}).pipe(
-        map(() => true),
-        catchError(() => of(false))
-    )
-  }
+    isAuthenticated(token: string): Observable<boolean> {
+        const headers = new HttpHeaders({
+            Authorization: `Bearer ${token}`
+        })
+        return this.http.get(`${STRAVA_BASE_URL}/api/v3/athlete`, { headers }).pipe(
+            map(() => true),
+            catchError(() => of(false))
+        )
+    }
 }
